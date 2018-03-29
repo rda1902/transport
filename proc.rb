b3e52096f59102b039d82793fc96c46af8e75b22
Dmitriy Rubekin <rda1902@gmail.com>
2018-03-29 14:15:56 +0300

    fix proc

--- proc.rb
+++ proc.rb
@@ -1,2 +1,23 @@
 require 'daemons'
-Daemons.run('transport.rb')
+require 'pg'
+require 'active_support/all'
+require 'geocoder'
+require 'colorize'
+require 'dotenv/load'
+require 'logger'
+require 'net/http'
+require 'redis'
+require 'oj'
+require 'benchmark'
+
+options = {
+  keep_pid_files: true,
+  dir_mode: :script,
+  dir: __dir__
+}
+
+Dir["#{File.dirname(__FILE__)}/lib/**/*.rb"].each { |file| require file }
+
+Daemons.run_proc('transport.rb', options) do
+  Estimate::TransportLogger.new.run
+end
